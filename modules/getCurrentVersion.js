08955ac9b362f8e14d8338b781debf8ea4d89eb9
jygttgyy <92969468+jygttgyy@users.noreply.github.com>
2024-11-10 13:42:11 +0100

    Delete modules directory

--- modules/getCurrentVersion.js
+++
@@ -1,9 +0,0 @@
-var currentVersion = 0;
-export const getCurrentVersion = () => {
-	return currentVersion.toString();
-}
-export const incrementCurrentVersion = () => {
-	currentVersion += 1;
-	if (currentVersion === 10) currentVersion = 0;
-	return currentVersion.toString();
-}
--- modules/userCooldown.js
+++
@@ -1,7 +0,0 @@
-var usersOnCooldown = [];
-export default (userID) => {
-	if (usersOnCooldown.includes(userID)) return true;
-	usersOnCooldown.push(userID);
-	setTimeout(() => {delete usersOnCooldown[usersOnCooldown.indexOf(userID)]}, 1000);
-	return false;
-};
