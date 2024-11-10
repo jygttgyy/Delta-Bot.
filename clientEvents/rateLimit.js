16c1aa0f67b1040a2044c103d7e25999d3b6ac9d
jygttgyy <92969468+jygttgyy@users.noreply.github.com>
2024-11-10 13:41:57 +0100

    Delete clientEvents directory

--- clientEvents/rateLimit.js
+++
@@ -1,8 +0,0 @@
-import { Events } from 'discord.js';
-
-export const event = Events.RateLimit;
-export const frequency = "on";
-export const onExecution = (info) => {
-	console.log("Rate-limited")
-	//console.log(info);
-}
